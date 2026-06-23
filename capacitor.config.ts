cf6f663cbc46b59146e621abf8438c3be0400e5d
Manfred JM Fornah <manfred657593@gmail.com>
2026-06-23 18:27:46 +0000

    Move GitHub Actions workflow to repository root

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'npx cap add androidnpx cap copy',
-  webDir: 'dist'
-};
-
-export default config;
